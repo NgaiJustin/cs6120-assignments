7f6a7f3a0167b1ae4058fd1d6fe334e9ee8b7585
Justin Ngai <jn537@cornell.edu>
2023-09-07 14:28:14 -0400

    Implement trivial dce

--- lesson-tasks/l3/dce.py
+++ lesson-tasks/l3/dce.py
@@ -4,6 +4,41 @@ import sys
 import argparse
 import json
 
+from collections import defaultdict
+from blocks import func_to_blocks, pprint_blocks
+from bril_type import *
+
+
+def dead_code_elimination(blocks: list[list[Instruction]]):
+    """Perform dead code elimination on a list of basic blocks."""
+    to_delete = defaultdict(
+        set
+    )  # Map of block indices to instruction indices to delete
+
+    for bi, block in enumerate(blocks):
+        last_def = {}  # Map of variable names to index of last def
+
+        for ii, instr in enumerate(block):
+            # Check for uses
+            for arg in instr.get("args", []):
+                last_def.pop(arg, None)  # Remove defs that are used
+
+            # Check for defs
+            if "dest" in instr:
+                if instr["dest"] in last_def:
+                    # Previous def is dead
+                    to_delete[bi].add(last_def[instr["dest"]])
+
+                last_def[instr["dest"]] = ii
+
+    # Batch delete dead instructions
+    for bi, to_delete_instrs_i in to_delete.items():
+        blocks[bi] = [
+            instr for ii, instr in enumerate(blocks[bi]) if ii not in to_delete_instrs_i
+        ]
+
+    return blocks
+
 
 def load():
     """Load a .bril program from the command line"""
@@ -30,13 +65,12 @@ def load():
         return
 
 
-def dead_code_elimination(program):
-    for func in program.get("functions", []):
-        for instr in func.get("instrs", []):
-            print(instr)
-
-
 if __name__ == "__main__":
     program = load()
     if program:
-        dead_code_elimination(program)
+        for func in program["functions"]:
+            basic_blocks = func_to_blocks(func)
+            pprint_blocks(basic_blocks)
+
+            # TODO: Measure the performance of dead code elimination
+            pprint_blocks(dead_code_elimination(basic_blocks))
