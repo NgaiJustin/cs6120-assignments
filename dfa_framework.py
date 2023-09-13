d2e600424d85b0218e41242728e7a53a4c9a356b
Justin Ngai <jn537@cornell.edu>
2023-09-13 17:30:38 -0400

    Update dfa_framework.py

--- dfa_framework.py
+++ dfa_framework.py
@@ -1,53 +1,39 @@
 from node import Node
 from collections import deque
+from dataclasses import dataclass
+
+from typing import Callable, Dict, Iterable, List
 
 
+@dataclass(frozen=True)
 class DataFlowAnalysis:
-    def __init__(
-        self,
-        cfg,
-        init_sets=None,
-        transfer_function=None,
-        merge_function=None,
-    ):
-        self.cfg = cfg
-        self.in_sets = {}
-        self.out_sets = {}
-        self.transfer_function = {}
-        self.merge_function = {}
-        self.init_sets()
+    entry_node: Node
+    in_sets: Dict[str, set]
+    out_sets: Dict[str, set]
+    transfer_function: Callable[[Node, set], set]
+    merge_function: Callable[[Iterable[set]], set]
 
-    def init_sets(self):
-        for node in self.cfg.nodes:
-            self.in_sets[node] = set()
-            self.out_sets[node] = set()
-            self.transfer_function[node] = lambda in_set: in_set
-            self.merge_function[node] = lambda in_sets: set.union(*in_sets)
-
-    def run(self):
+    def run(self: "DataFlowAnalysis") -> None:
         # Implement worklist algorithm with an initial DFS pass
-        worklist = deque()
-        changed = True
-        while changed:
-            changed = False
-            for node in self.cfg.nodes:
-                in_set = self.in_sets[node]
-                out_set = self.out_sets[node]
-                transfer_function = self.transfer_function[node]
-                merge_function = self.merge_function[node]
+        worklist: deque[Node] = deque([self.entry_node])
+        iters = 0
 
-                new_in_set = merge_function(
-                    [self.out_sets[pred] for pred in self.cfg.predecessors(node)]
-                )
-                new_out_set = transfer_function(in_set, node)
+        while worklist:
+            node = worklist.popleft()
 
-                if new_in_set != in_set or new_out_set != out_set:
-                    changed = True
-                    self.in_sets[node] = new_in_set
-                    self.out_sets[node] = new_out_set
+            in_set = self.in_sets[node.id]
+            out_set = self.out_sets[node.id]
 
-    def get_in_set(self, node: Node):
-        return self.in_sets[node]
+            new_in_set = self.merge_function(
+                [self.out_sets[pred.id] for pred in node.predecessors]
+            )
+            new_out_set = self.transfer_function(node, in_set)
 
-    def get_out_set(self, node: Node):
-        return self.out_sets[node]
+            if new_in_set != in_set or new_out_set != out_set:
+                self.in_sets[node.id] = new_in_set
+                self.out_sets[node.id] = new_out_set
+                worklist.extend(node.successors)
+
+            iters += 1
+
+        print(f"Ran {iters} iterations")
