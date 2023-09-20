53939e5e7eb9dbc60951dea0ac23d734f77f629d
Justin Ngai <jn537@cornell.edu>
2023-09-19 21:15:29 -0400

    Generate new dom visualization

--- dominator.py
+++ dominator.py
@@ -226,7 +226,7 @@ if __name__ == "__main__":
                 )
         else:
             # visualize animation for dominance relation for all nodes in CFG
-            name = "loopcond"
+            name = "small"
             dfs = DotFilmStrip(name)
             dfs.extend_frames(
                 [
---
+++ slow.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# first argument is the input file
+INPUT_FILE=$1
+
+# output file name is the input_file name with "-slow" appended before the extension
+OUTPUT_FILE=$(echo $INPUT_FILE | sed 's/\(.*\)\.\(.*\)/\1-slow.\2/')
+
+# 1. get input frame count (note two spaces after the "\")
+count=$(gifsicle --info < $INPUT_FILE | grep "<stdin>" | cut -d\  -f 3)
+
+# 2. define how many times to repeat frames
+repeats=10
+
+# 3. construct new frame list
+frames=$(for i in $(seq 0 $((count - 1)))
+    do 
+        for j in $(seq $((repeats)))
+            do echo "#$i"
+        done
+    done
+)
+
+# 4. use gifsicle to duplicate frames (note no quotes around $frames)
+gifsicle < $INPUT_FILE $frames > $OUTPUT_FILE
+
+
+# Source: https://graphicdesign.stackexchange.com/questions/137443/duplicate-frames-in-animated-gif
