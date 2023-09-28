ec95356eb639bb81aca6663a63f10a89523762d7
Justin Ngai <jn537@cornell.edu>
2023-09-28 02:48:32 -0400

    Update to_ssa implementation

--- ssa.py
+++ ssa.py
@@ -16,7 +16,7 @@ def _collect_vars(entry_node: Node) -> Dict[str, Set[Node]]:
     """
     Return a mapping of variables to the nodes in which they are defined/assigned .
     """
-    var_to_assigns: Dict[str, Set[Node]] = defaultdict(set)
+    var_to_assignments: Dict[str, Set[Node]] = defaultdict(set)
     q = deque([entry_node])
     while q:
         node = q.popleft()
@@ -24,24 +24,27 @@ def _collect_vars(entry_node: Node) -> Dict[str, Set[Node]]:
         # assignment statement
         if "dest" in node.instr:
             var_name = node.instr["dest"]
-            var_to_assigns[var_name].add(node)
+            var_to_assignments[var_name].add(node)
 
         q.extend(node.successors)
 
-    return var_to_assigns
+    return var_to_assignments
 
 
 def _rename_vars(node: Node) -> None:
     pass
 
 
-def to_ssa(entry_node: Node) -> List[Node]:
+def to_ssa(entry_node: Node) -> None:
     """
     Convert a CFG into SSA form.
     """
-    var_to_assigns = _collect_vars(entry_node)
-    for var in var_to_assigns.keys():
-        for node in var_to_assigns[var]:
+    var_to_assignments = _collect_vars(entry_node)
+
+    for var in var_to_assignments.keys():
+        assignments_q = deque(var_to_assignments[var])
+        while assignments_q:
+            node = assignments_q.popleft()
             for df_node in dominance_frontier(node, entry_node):
                 # add phi node to df_node
                 if df_node.phi_node is None:
@@ -49,9 +52,19 @@ def to_ssa(entry_node: Node) -> List[Node]:
                 df_node.phi_node.add_var(var, node.id, var)
 
                 # update assignments to include the phi node use case
-                var_to_assigns[var].add(df_node)
+                if df_node not in var_to_assignments[var]:
+                    var_to_assignments[var].add(df_node)
+                    assignments_q.append(df_node)
 
-    return []
+    # traverse all nodes and rename variables
+    q = deque([entry_node])
+    seen: Set[str] = set()
+
+    while q:
+        curr_node = q.popleft()
+        _rename_vars(curr_node)
+        seen.add(curr_node.id)
+        q.extend([succ for succ in curr_node.successors if succ.id not in seen])
 
 
 def from_ssa(cfg_nodes: List[Node]) -> List[Node]:
@@ -69,22 +82,23 @@ def validate_ssa(cfg_nodes: List[Node]) -> bool:
 
 
 if __name__ == "__main__":
-    program, cli_flags = load(["-t", "-f", "-v"])
+    program, cli_flags = load(["-to", "-from", "-check"])
 
     if program is None:
         sys.exit(1)
 
-    if not cli_flags["t"] and not cli_flags["f"]:
-        print("Please specify either -t or -f")
+    if not cli_flags["to"] and not cli_flags["from"]:
+        print("Please specify either: \n  ... ssa.py -to \n  ... ssa.py -from")
         sys.exit(1)
 
-    cfg_nodes = to_cfg_fine_grain(program)
-    entry_nodes = get_entry_nodes(cfg_nodes)
+    cfg_root_nodes = to_cfg_fine_grain(program)
 
-    if cli_flags["to_ssa"]:
-        pass
+    if cli_flags["to"]:
+        for root_node in cfg_root_nodes:
+            to_ssa(root_node.entry_node)
+            print(visualize(root_node.entry_node))
 
-    elif cli_flags["from_ssa"]:
+    elif cli_flags["from"]:
         if cli_flags["-check"]:
             # Validate that the program is in SSA form
             pass
