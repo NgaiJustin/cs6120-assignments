5de8bfd78932339fe2a3a9ba3e0fc073b4611db7
Justin Ngai <jn537@cornell.edu>
2023-09-28 16:51:38 -0400

    Implement var rename without phi node

--- ssa.py
+++ ssa.py
@@ -1,13 +1,14 @@
 """
 A series of utility functions for transforming BRIL programs into and out of SSA form.
 """
+import json
 import sys
 from collections import defaultdict, deque
 from typing import Dict, List, Set
 
 from bril_type import *
 from cfg import get_entry_nodes, to_cfg_fine_grain
-from dominator import dominance_frontier
+from dominator import _get_dominators, dominance_frontier, dominance_tree
 from node import Node, PhiNode, visualize
 from utils import load
 
@@ -33,8 +34,47 @@ def _collect_vars(entry_node: Node) -> Dict[str, Set[Node]]:
     return var_to_assignments
 
 
-def _rename_vars(node: Node) -> None:
-    pass
+def _rename_vars(entry_node: Node, dom_tree_dict: Dict[Node, Set[Node]]) -> None:
+    var_stack: Dict[str, deque] = defaultdict(deque)
+    var_counter: Dict[str, int] = defaultdict(int)
+
+    def _get_new_name(var: str) -> str:
+        new_name = f"{var}_{var_counter[var]}"
+        var_counter[var] += 1
+        var_stack[var].append(new_name)
+        return new_name
+
+    def _rename(node: Node) -> None:
+        # deep copy of var_stack
+        var_stack_cache = {var_name: q.copy() for var_name, q in var_stack.items()}
+
+        # rename phi_node destination??
+
+        if "args" in node.instr:
+            # replace args
+            new_args = [var_stack[arg][-1] for arg in node.instr["args"]]
+            node.instr["args"] = new_args
+
+        if "dest" in node.instr:
+            # replace dest
+            old_dest = node.instr["dest"]
+            new_dest = _get_new_name(old_dest)
+            node.instr["dest"] = new_dest
+
+            # only update phi nodes if var has new assignment
+            for succ in node.successors:
+                if succ.phi_node is not None:
+                    succ.phi_node.update_var(new_dest, old_dest, node.id)
+
+        # rename all immediately dominated nodes
+        for im_dom_node in dom_tree_dict[node]:  # TODO: FIX!)
+            _rename(im_dom_node)
+
+        # restore var_stack
+        var_stack.clear()
+        var_stack.update(var_stack_cache)
+
+    _rename(entry_node)
 
 
 def to_ssa(entry_node: Node) -> None:
@@ -58,15 +98,11 @@ def to_ssa(entry_node: Node) -> None:
                     var_to_assignments[var].add(df_node)
                     assignments_q.append(df_node)
 
-    # traverse all nodes and rename variables
-    q = deque([entry_node])
-    seen: Set[str] = set()
+    doms = _get_dominators(entry_node)
+    dom_tree = dominance_tree(doms)
+    dom_tree_dict = {node: node.successors for node in dom_tree}
 
-    while q:
-        curr_node = q.popleft()
-        _rename_vars(curr_node)
-        seen.add(curr_node.id)
-        q.extend([succ for succ in curr_node.successors if succ.id not in seen])
+    _rename_vars(entry_node, dom_tree_dict)
 
 
 def from_ssa(cfg_nodes: List[Node]) -> List[Node]:
